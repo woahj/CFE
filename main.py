59e720016425f4ddd7e08bf1800e531f1535ce76
woahj <89588015+woahj@users.noreply.github.com>
2022-10-18 09:04:37 -0500

    grab from heroku variables
    idk if this will work lo

--- main.py
+++ main.py
@@ -2,13 +2,18 @@ from time import sleep
 from Logger.Logger import Logger
 from Config import Config
 from Browser import Browser
+from boto.s3.connection import S3Connection
 
 
 log = Logger().createLogger()
 config = Config()
 browser = Browser(log, config)
 
-if browser.login(config.username, config.password):
+user = S3Connection(os.environ['user']
+passw = S3Connection(os.environ['pass']
+
+#is it obvious i dont know how to code
+if browser.login(user, passw):
     log.info("Successfully logged in")
     while True:
         try:
